859b880c2a55f3da6645bd2b7e96ba4808e7541a
alfarelrizky <alfarelrizky99@gmail.com>
2020-07-23 22:50:08 +0700

    bersihin project lama

--- resources/views/layouts/navigation.blade.php
+++
@@ -1,3 +0,0 @@
-<a href='/dashboard'>Dashboard</a>
-<a href='/agenda'>Agenda</a>
-<a href='/table'>Table</a>
--- resources/views/tabledata.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts/app')
-
-@section('content')
-<h1>Table Data</h1>
-@endsection
